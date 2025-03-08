3f3171b951b2c02db6dafa052de0a0c6328a4b57
Lorenzo <esempio@esempio.com>
2025-03-08 10:35:16 +0100

    resolved conflicts

--- src/main/java/org/mio/progettoingsoft/advCards/Epidemic.java
+++ src/main/java/org/mio/progettoingsoft/advCards/Epidemic.java
@@ -3,7 +3,7 @@ package org.mio.progettoingsoft.advCards;
 import org.mio.progettoingsoft.AdventureCard;
 
 public class Epidemic extends AdventureCard {
-    protected Epidemic(int level) {
-        super(level);
+    protected Epidemic(int id, int level) {
+        super(id, level);
     }
 }
--- src/main/java/org/mio/progettoingsoft/advCards/Sabotage.java
+++ src/main/java/org/mio/progettoingsoft/advCards/Sabotage.java
@@ -3,7 +3,7 @@ package org.mio.progettoingsoft.advCards;
 import org.mio.progettoingsoft.AdventureCard;
 
 public class Sabotage extends AdventureCard {
-    protected Sabotage(int level) {
-        super(level);
+    protected Sabotage(int id,int level) {
+        super(id, level);
     }
 }
