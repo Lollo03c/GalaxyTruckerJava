972449208f90c737243c5486737407d0cf0c6b04
Stefano Bernardotto <stefano.bernardotto@mail.polimi.it>
2025-05-06 09:13:21 +0200

    done planets

--- src/main/java/org/mio/progettoingsoft/advCards/sealed/SldAdvCard.java
+++ src/main/java/org/mio/progettoingsoft/advCards/sealed/SldAdvCard.java
@@ -16,12 +16,17 @@ public abstract sealed class SldAdvCard permits SldAbandonedShip, SldEpidemic, S
     protected CardState state;
 
     public abstract String getCardName();
+
     // this method must be called right after drawing the card, it sets the game state CARD_EFFECT
     public abstract void init(FlyBoard board);
+
     // this method must be called right after applying the effect of the card on ALL the available players, it sets the game state DRAW_CARD
     public abstract void finish(FlyBoard board);
+
     public CardState getState() {return state;}
+
     public Player getActualPlayer() {return actualPlayer;}
+
     public void keepShipPart(FlyBoard board, Player player, int row, int col) {
         if(this.state != CardState.PART_CHOICE){
             throw new IllegalStateException("Illegal state: " + this.state);
