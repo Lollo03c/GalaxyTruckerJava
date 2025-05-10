5ffdfc98b76568432d34bb4e2e55e379c7aee7bc
Lorenzo <lorenzobaggi03@gmail.com>
2025-05-10 21:12:20 +0200

    tui for shipboard

---
+++ src/main/java/org/mio/progettoingsoft/views/ColoredChar.java
@@ -0,0 +1,30 @@
+package org.mio.progettoingsoft.views;
+
+public class ColoredChar {
+    public char c;
+    public String color;
+    public static final String WHITE = "\u001B[37m";
+
+    public ColoredChar(char c) {
+        this.c = c;
+        color = WHITE;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setChar(char c){
+        this.c = c;
+    }
+    public ColoredChar(char c, String color) {
+        this.c = c;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        final String RESET = "\u001B[0m";
+        return color + c + RESET;
+    }
+}
+
